f73e6dc319a6b2e0ae9b09278b498555785c6af7
Shrey456 <82593769+Shrey456@users.noreply.github.com>
2021-07-13 19:27:38 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,29 @@
+var dustbin1,dustbin2,dustbin3;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	dustbin1 = createSprite(400,650,200,20);
+	dustbin2 = createSprite(300,610,20,100);
+	dustbin3 = createSprite(500,610,20,100);
+	
+}
+function draw(){
+
+
+}
+drawSprites()
+
+funtion keypressed()
+{
+
+
+} 
